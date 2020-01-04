9db9330d68f370917a7d49af1cecc4acd680a397
danailstratirv <stratyd@abv.bg>
2020-01-04 05:17:48 +0200

    Calorie Object

---
+++ JS Advanced/JS Syntax, Functions and Statements/Exercise05.CalorieObject.js
@@ -0,0 +1,12 @@
+function calorieObject(input) {
+    let obj = {};
+
+    for (let i = 0; i < input.length; i+=2) {
+
+        obj[input[i]] = +input[i+1];
+    }
+
+    console.log(obj);
+}
+
+calorieObject(['Yoghurt', 48, 'Rise', 138, 'Apple', 52]);
