36f5e0c4b1b08088aa2eaba07f6231465ab04fdf
danailstratirv <stratyd@abv.bg>
2020-01-14 22:34:42 +0200

    Chat Room

---
+++ JS Advanced/03.Objects and DOM/DOM/01. Chat-Room/solution.js
@@ -0,0 +1,31 @@
+function solve() {
+
+   let targetDiv = document.getElementsByClassName('my-message')[0];
+   let sendBtn = document.getElementById('send');
+   let textArea = document.getElementById('chat_input');
+   let chatMessageArea = document.getElementById('chat_messages');
+
+
+
+   sendBtn.addEventListener('click', function () {
+      let targetDivClone = targetDiv.cloneNode(true);
+
+      let textAreaContent = textArea.value;
+
+      if (textAreaContent !== '') {
+      targetDivClone.textContent = textAreaContent;
+
+      chatMessageArea.appendChild(targetDivClone);
+
+      textArea.value = '';
+      }else {
+         let allContent = document.body;
+         let errorElement = document.createElement("h1");
+      }
+      
+   });
+
+
+}
+
+
