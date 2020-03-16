d2490591ef567bf67e7e95685796062d9c7c1335
danailstratirv <stratyd@abv.bg>
2020-03-16 14:24:10 +0200

    Bus Schedule

---
+++ JS Applications/Rest Rest Services and AJAX/02.BUS-SCHEDULE/app.js
@@ -0,0 +1,38 @@
+function solve() {
+
+    let baseUrl = 'https://judgetests.firebaseio.com/schedule/';
+    let currentStopId = '0098';
+    let currentStop = '';
+
+    function depart() {
+        let url = baseUrl + currentStopId + '.json';
+
+        fetch(url)
+        .then(request => request.json())
+        .then((data) => loadStop(data));
+    }
+
+    function loadStop(data){
+        currentStop = data;
+        document.querySelector('span.info').textContent = `Next stop ${currentStop.name}`;
+        currentStopId = currentStop.next;
+        document.getElementById('depart').setAttribute('disabled', true);
+        document.getElementById('arrive').disabled = false;        
+
+    }
+
+    function arrive() {
+        document.querySelector('span.info').textContent = `Arriving at ${currentStop.name}`;
+        currentStopId = currentStop.next;
+        document.getElementById('depart').disabled = false;
+        document.getElementById('arrive').disabled = true;        
+
+    }
+
+    return {
+        depart,
+        arrive
+    };
+}
+
+let result = solve();
